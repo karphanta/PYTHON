2b039468c8cebcc6396abc16d96c9cd43b4febea
karphanta <osejjose@gmail.com>
2025-10-13 13:04:57 +0200

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,31 @@
+# script_malo.py
+
+import os  # Importación no utilizada
+
+def funcion_mala():
+    a = 5
+    b = 10
+    c = a + b  # Variable 'c' nunca se usa
+
+    if a > 0:
+        pass  # Bloque vacío
+
+    if b < 20:
+        print("b es menor que 20")
+    else:
+        # ELSE vacío
+        pass
+
+    # Comentario inútil
+    # TODO: arreglar esto algún día
+
+    print("Resultado:", c)  # 'c' no se usa antes
+
+def otra_funcion():
+    for i in range(10):
+        break  # Bucle innecesario
+
+    return 42
+    return 99  # Código muerto
+
+funcion_mala()
